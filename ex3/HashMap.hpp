c10d20c0ebeebf1e8c1fd1a2752ea578c21dc503
ahmad.shakla <ahmad.shakla@mail.huji.ac.il>
2019-09-15 13:44:14 +0300

    new

---
+++ ex3/HashMap.hpp
@@ -0,0 +1,15 @@
+//
+// Created by ahmad.shakla on 9/15/19.
+//
+
+#ifndef EX3_HASHMAP_HPP
+#define EX3_HASHMAP_HPP
+
+
+class HashMap
+{
+
+};
+
+
+#endif //EX3_HASHMAP_HPP
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    sdfsdfsdfsf
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
