082b99a99ce0792b514796423722c44215024c44
ahmad.shakla <ahmad.shakla@mail.huji.ac.il>
2019-09-15 21:58:01 +0300

    new

--- ex3/HashMap.hpp
+++ ex3/HashMap.hpp
@@ -1,15 +1,28 @@
 //
 // Created by ahmad.shakla on 9/15/19.
 //
+#include <iostream>
+#include <bits/unique_ptr.h>
 
-#ifndef EX3_HASHMAP_HPP
-#define EX3_HASHMAP_HPP
+#ifndef CPP_EX3_HPP
+#define CPP_EX3_HPP
 
-
+template <typename KeyT,typename ValueT>
 class HashMap
 {
+public:
+    KeyT key;
+    ValueT value;
+    
+
 
 };
 
+int main()
+{
+    HashMap<int,double> hashMap(3,2);
+    hashMap.value1 = 2.3;
+    hashMap.key1 = 1;
 
+}
 #endif //EX3_HASHMAP_HPP
